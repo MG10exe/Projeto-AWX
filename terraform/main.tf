de055b48025da336f00f7b80f39968b7addc191b
MG10exe <matheusgalucena@hotmail.com>
2024-12-01 12:37:27 -0300

    Updating terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.32.1"
-    }
 
     google = {
       source = "hashicorp/google"
@@ -11,34 +7,19 @@ terraform {
     }
   }
 
-  backend "s3" {}
+  backend "gcs" {}
 }
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "tf-demo-aws-ec2-instance-1" {
-  ami           = "ami-0005e0cfe09cc9050"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "tf-demo-aws-ec2-instance-1"
-  }
-}
-
-variable "gcp_credentials_path" { type= string }
-variable "gcp_project" { type= string }
 
 provider "google" {
   credentials = var.gcp_credentials_path
   project = var.gcp_project
-  region = "northamerica-northeast1"
+  region = "us-east-1"
 }
 
-resource "google_compute_instance" "tf-demo-gcp-instance-1" {
-  name         = "tf-demo-gcp-instance-1"
-  machine_type = "e2-micro"
-  zone = "northamerica-northeast1-a"
+resource "google_compute_instance" "maquina_teste" {
+  name = "maquina-teste"
+  machine_type = var.instance_type["medium"]
+  zone = "us-central1-c"
 
   boot_disk {
     initialize_params {
@@ -49,7 +30,10 @@ resource "google_compute_instance" "tf-demo-gcp-instance-1" {
     }
   }
 
-network_interface {
+  network_interface {
     network = "default"
+    access_config {}
   }
+
+  tags = ["http-server", "https-server"]
 }
