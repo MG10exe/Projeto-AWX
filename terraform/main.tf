da669cceebbeb985796ebd3d7daf3d4d133fc378
Matheus Gomes <61914401+MG10exe@users.noreply.github.com>
2025-02-06 12:22:13 -0300

    fix secret name

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ provider "google" {
 }
 
 data "google_secret_manager_secret_version" "chave_publica" {
-  secret  = "chave-publica-awxServer"
+  secret  = "chave-publica-servidor-awx"
   project = var.gcp_project
 }
 
