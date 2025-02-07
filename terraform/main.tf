c95d1c883684adaa22e7a2aca3669c6b1bd61709
Matheus Gomes <61914401+MG10exe@users.noreply.github.com>
2025-02-07 08:51:36 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,6 +58,8 @@ resource "google_service_networking_connection" "default" {
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
 
+  depends_on = [google_compute_global_address.private_ip_alloc]
+
 }
 
 # Sub-rede Pública
